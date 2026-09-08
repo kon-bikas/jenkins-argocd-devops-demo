392bbfb6a5559e7a9ad425697de486b0ed01be26
kbikas <kostasbikas02@gmail.com>
2026-09-08 20:21:57 +0300

    Implemented the simplest golang server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kon-bikas/demo-devops-app
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	resp := map[string]string{
+		"version": "1.0.0",
+		"message": "hello",
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	http.ListenAndServe(":8080", nil)
+}
